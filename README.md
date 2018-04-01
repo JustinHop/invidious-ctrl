7af5b36f704c76949c628abb6f1460fe92ca6509
Stefan <stfnhh@pm.me>
2018-04-01 11:20:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+HookTube Ctrl - HookTube Chrome Extension
+==========
+
+Adds keyboard controls to HookTube, uses the same mapping is YouTube.
+
+**F** - Toggle Fullscreen 
+**M** - Toggle Mute 
+**0-9** - Seek to the 10% / 90% of the video
+**F** - Toggle Fullscreen 
+**K** - Toggle Play / Pause 
+**J** - Rewind 10 seconds
+**L** - Fast Forward 10 seconds
+
+Installing
+-----
+
+Download or clone this code and load as an unpacked extension.
+
+Source Code: [github.com/stfnhh/hooktube-ctrl](http://github.com/stfnhh/hooktube-ctrl)
+
+
+Unpacked Extension
+-----
+
+- If you downloaded the code, unzip the file.
+- Open (chrome://extensions/) or select the menu `Window > Extensions`.
+- Enable the developer mode at top right.
+- Click `Load unpacked extension...` and select the source code folder.
+
+
+Issues
+-----
+N/A
+
+Found a new one, please let me know: [issue tracker](http://github.com/stfnhh/hooktube-ctrl/issues).
+
+
+Contributing
+-----
+
+Fork at it.
+
+
+License
+-----
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ main.js
@@ -0,0 +1,51 @@
+const controls = (player) => {
+  var player = document.querySelector(player);
+
+  document.addEventListener('keydown', (e) => {
+
+    // Don't fire keyboard controls if focus is in search field
+    if(document.querySelector('#search-input') === document.activeElement) {
+      return(false);
+    }
+
+    let key = e.keyCode;
+
+    switch (true) {
+      case(key == 70): // F
+        document.webkitIsFullScreen ? player.webkitExitFullscreen() : player.webkitEnterFullscreen();
+        break;
+      case(key >= 48 && key <= 57): // 0-9
+        player.currentTime = Math.round(player.duration*((key-48)/10));
+        break;
+      case(key == 75): // K
+        player.paused ? player.play() : player.pause();
+        break;
+      case(key == 39): // right arrow
+        player.currentTime += 5;
+        break;
+      case(key == 37): // left arrow
+        player.currentTime -= 5;
+        break;
+      case(key == 74): // J
+        player.currentTime -= 10;
+        break;
+      case(key == 76): // L
+        player.currentTime += 10;
+        break;
+      case(key == 77): // K
+        player.volume == 0 ? player.volume = 1 : player.volume = 0;
+        break;
+    }
+  }, false);
+}
+
+// Wait for HookTube player to be ready
+const observer = new MutationObserver(function(mutations) {
+  if (document.contains(document.querySelector('#player-obj'))) {
+    document.activeElement.blur();
+    controls('#player-obj');
+    observer.disconnect();
+  }
+});
+
+observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
